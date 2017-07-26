56786d5502b2de59054645156d9295e9c7d6f03b
Jan Mueller <mue.jv@freenet.de>
2017-07-26 15:33:03 +0200

    Rotate

---
+++ githubtest/Assets/Scripts/Rotate.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+
+
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.Rotate(1f, 0f, 0f);
+	}
+}
