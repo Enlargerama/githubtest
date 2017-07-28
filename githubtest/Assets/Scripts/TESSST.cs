d8d29a90efb0043a0da2260ffae616336bfcfa5a
Jan Mueller <mue.jv@freenet.de>
2017-07-28 15:27:38 +0200

    test.cs

---
+++ githubtest/Assets/Scripts/TESSST.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TESSST : MonoBehaviour {
+
+
+    Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = this.GetComponent<Rigidbody>();
+        rb.useGravity = false;
+        rb.AddForce(50f, 0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
